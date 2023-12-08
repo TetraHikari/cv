a201837e6b5030d282add679eeec136ede7e9168
TetraHikari <111183374+TetraHikari@users.noreply.github.com>
2023-12-09 02:38:20 +0700

    add animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Home() {
+    
     return (
-        <div className="Home">
-        <div className="Home-header">
-            <div>PEERANAT PATARAKITTISOPOL</div>
-        </div>
-        <div className="Home-content">
-            <div>2nd year  software engineer student at KMITL. I enjoyed learning new things since when i was young especially in technologies. I mainly do the software works and also do some level of hardware</div>
-        </div>
+        <div className="Home ml-20 mr-20 mt-10 w-fit">
+            <div className="Home-header flex justify-start mb-10 animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black pr-5 text-5xl text-black font-bold w-50">
+                <div>I'M PEERANAT PATARAKITTISOPOL</div>
+            </div>
+            <div className="Home-content flex justify-start">
+                <div>2nd year  software engineer student at KMITL. I enjoyed learning new things since when i was young especially in technologies. I mainly do the software works and also do some level of hardware</div>
+            </div>
         </div>
     );
 }
