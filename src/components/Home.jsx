f7ba4f7967a7ebd3fb01a5204ebd343f3a83fd7d
TetraHikari <111183374+TetraHikari@users.noreply.github.com>
2023-12-10 02:38:38 +0700

    bg css + minor adjust

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ function Home() {
     
     return (
         <div className="Home mt-10 w-max text">
-            <div className="flex-row w-fit">
-                <div className="flex justify-start animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black pr-5 text-5xl text-black font-bold">I'M PEERANAT PATARAKITTISOPOL</div>
+            <div className="flex-row w-fit mb-10">
+                <div className="flex justify-start animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black pr-5 text-5xl text-black font-bold">PEERANAT PATARAKITTISOPOL</div>
             </div>
             <div className="Home-content flex justify-start ">
                 <div>2nd year  software engineer student at KMITL. I enjoyed learning new things since when i was young especially in technologies. I mainly do the software works and also do some level of hardware</div>
